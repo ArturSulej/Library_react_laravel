fe28c392c28aa24f1fc1b03cb226d161dcc84b41
ArturSulej <sulejartur2@gmail.com>
2022-06-08 15:27:59 +0200

    timestamp 7:24

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/books', [BookController::class, 'index']);
+
+Route::post('/books',function(){
+    //return Book::create([
+    //    'title' => 'Book One',
+    //    'description' => 'Description One',
+    //    'author' => 'Author One',
+    //    'category' => '1',
+    //    'slug' => 'book-one'
+    //]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
